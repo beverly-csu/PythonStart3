f245fbe220e510011568108ab87b755586d14fe7
a4six <beverly_csu@mail.ru>
2022-10-30 14:23:19 +0500

    Update app.py

--- MemoryCard/app.py
+++ MemoryCard/app.py
@@ -5,6 +5,7 @@ from PyQt5.QtWidgets import QGroupBox, QButtonGroup
 from random import shuffle
 
 
+# Функции
 def show_result():
     grpbox_answers.hide()
     grpbox_result.show()
@@ -21,25 +22,6 @@ def show_question():
     grpbox_result.hide()
     btn_ok.setText('Ответить')
 
-app = QApplication([])
-window = QWidget()
-
-lbl_question = QLabel('Тут будет будующий вопрос)))')
-btn_ok = QPushButton('Ответить')
-
-grpbox_answers = QGroupBox('Варианты ответов:')
-
-btn_group = QButtonGroup()
-rbtn_1 = QRadioButton('1')
-rbtn_2 = QRadioButton('2')
-rbtn_3 = QRadioButton('3')
-rbtn_4 = QRadioButton('4')
-btn_group.addButton(rbtn_1)
-btn_group.addButton(rbtn_2)
-btn_group.addButton(rbtn_3)
-btn_group.addButton(rbtn_4)
-
-answers = [rbtn_1, rbtn_2, rbtn_3, rbtn_4]
 def ask(question, right_answer, wrong1, wrong2, wrong3):
     lbl_question.setText(question)
     shuffle(answers)
@@ -54,9 +36,40 @@ def start_test():
         show_result()
     else:
         ask('Кто создал Python?', 'Гвидо Ван Россум', 'Билл Гейтс', 'Марк Цукенберг', 'Стив Джобс')
+# Функции
+
+app = QApplication([])
+window = QWidget()
+
+lbl_question = QLabel('Тут будет будующий вопрос)))')
+btn_ok = QPushButton('Ответить')
+
+# Создание GroupBox'a с вариантами ответов
+grpbox_answers = QGroupBox('Варианты ответов:')
+btn_group = QButtonGroup()
+rbtn_1 = QRadioButton('1')
+rbtn_2 = QRadioButton('2')
+rbtn_3 = QRadioButton('3')
+rbtn_4 = QRadioButton('4')
+btn_group.addButton(rbtn_1)
+btn_group.addButton(rbtn_2)
+btn_group.addButton(rbtn_3)
+btn_group.addButton(rbtn_4)
+answers = [rbtn_1, rbtn_2, rbtn_3, rbtn_4]
+# Создание GroupBox'a с вариантами ответов
+
+# Создание GroupBox'a с результатом
+grpbox_result = QGroupBox('Результаты теста')
+lbl_result = QLabel('Правильный ответ: 123')
+v_line_result = QVBoxLayout()
+v_line_result.addWidget(lbl_result)
+grpbox_result.setLayout(v_line_result)
+grpbox_result.hide()
+# Создание GroupBox'a с результатом
 
 btn_ok.clicked.connect(start_test)
 
+# Создание и установки layout'ов
 h_line_ans = QHBoxLayout()
 v_line_ans_1, v_line_ans_2 = QVBoxLayout(), QVBoxLayout()
 
@@ -68,13 +81,6 @@ v_line_ans_2.addWidget(rbtn_4)
 h_line_ans.addLayout(v_line_ans_1)
 h_line_ans.addLayout(v_line_ans_2)
 grpbox_answers.setLayout(h_line_ans)
-
-grpbox_result = QGroupBox('Результаты теста')
-lbl_result = QLabel('Правильный ответ: 123')
-v_line_result = QVBoxLayout()
-v_line_result.addWidget(lbl_result)
-grpbox_result.setLayout(v_line_result)
-grpbox_result.hide()
 
 v_line_main = QVBoxLayout()
 h_line_main_1 = QHBoxLayout()
@@ -93,6 +99,7 @@ v_line_main.addLayout(h_line_main_2)
 v_line_main.addLayout(h_line_main_3)
 
 window.setLayout(v_line_main)
+# Создание и установки layout'ов
 
 window.show()
 app.exec()
