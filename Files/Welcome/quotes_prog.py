214424ceab559d3ceb8b6c055947094c280f96ad
a4six <beverly_csu@mail.ru>
2022-11-13 14:50:04 +0500

    Update quotes_prog.py

--- Files/Welcome/quotes_prog.py
+++ Files/Welcome/quotes_prog.py
@@ -7,11 +7,13 @@ with open('quotes.txt', 'a', encoding='utf-8') as file:
     text = '(' + author + ')\n'
     file.write(text)
 
+authors = [author]
 choose = input('Хотите ли добавить новую цитату? ')
 while choose != 'нет':
     with open('quotes.txt', 'a', encoding='utf-8') as file:
         lyrics = input('Введите цитату: ')
         author = input('Введите автора: ')
+        authors.append(author)
         text = lyrics + '\n(' + author + ')\n'
         file.write(text)
     choose = input('Хотите ли добавить новую цитату? ')
