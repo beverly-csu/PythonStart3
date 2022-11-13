bb57f61dae0ca8eaaf616733d64638887e6f9de7
a4six <beverly_csu@mail.ru>
2022-11-13 15:31:23 +0500

    Update pupils_small.py

--- Files/Class/pupils_small.py
+++ Files/Class/pupils_small.py
@@ -14,3 +14,11 @@ with open('pupils_txt.txt', 'r', encoding='utf-8') as file:
         pupils.append(pupil)
         print(data[0], data[1], '-', int(data[2]))
 
+sum_of_marks = 0
+print('Отличники:')
+for pupil in pupils:
+    sum_of_marks += pupil.mark
+    if pupil.mark == 5:
+        print(pupil.surname)
+
+print('Средняя оценка класса:', sum_of_marks / len(pupils))
