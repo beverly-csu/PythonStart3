1c1d22e3f2af3739ca781190788b24cbbddc7158
a4six <beverly_csu@mail.ru>
2022-11-13 14:39:12 +0500

    Update quotes_prog.py

--- Files/Welcome/quotes_prog.py
+++ Files/Welcome/quotes_prog.py
@@ -1,3 +1,8 @@
 #напиши здесь свою программу
 with open('quotes.txt', 'r', encoding='utf-8') as file:
     print(file.read())
+
+author = input('Введите автора данного отрывка:')
+with open('quotes.txt', 'a', encoding='utf-8') as file:
+    text = '(' + author + ')\n'
+    file.write(text)
