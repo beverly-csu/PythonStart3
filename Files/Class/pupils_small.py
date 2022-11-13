cd6c5c798dbed07722d9fa76c4025357a846d724
a4six <beverly_csu@mail.ru>
2022-11-13 15:25:50 +0500

    Update pupils_small.py

--- Files/Class/pupils_small.py
+++ Files/Class/pupils_small.py
@@ -1,1 +1,16 @@
 #напиши здесь свою программу
+class Pupil:
+    def __init__(self, surname, name, mark):
+        self.surname = surname
+        self.name = name
+        self.mark = mark
+
+pupils = []
+
+with open('pupils_txt.txt', 'r', encoding='utf-8') as file:
+    for line in file:
+        data = line.split(' ')
+        pupil = Pupil(data[0], data[1], int(data[2]))
+        pupils.append(pupil)
+        print(data[0], data[1], '-', int(data[2]))
+
