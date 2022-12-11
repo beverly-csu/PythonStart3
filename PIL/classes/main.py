d1ecc641488e14fb5b2eb29e926ec982d51165da
beverly-csu <80579574+beverly-csu@users.noreply.github.com>
2022-12-11 15:20:01 +0500

    Create main.py

---
+++ PIL/classes/main.py
@@ -0,0 +1,22 @@
+from PIL import Image, ImageFilter
+
+class ImageEditor:
+    def __init__(self, filename):
+        self.filename = filename
+        self.original = None
+        self.changed = []
+
+    def open(self):
+        try:
+            self.original = Image.open(self.filename)
+            self.original.show()
+        except:
+            print('Такого изображения нету :(')
+
+    def do_bw(self):
+        if self.original is not None:
+            gray = self.original.convert('L')
+            self.changed.append(gray)
+            old = self.filename.split('.')
+            new = old[0] + '_bw.' + old[1]
+            gray.save(new)
