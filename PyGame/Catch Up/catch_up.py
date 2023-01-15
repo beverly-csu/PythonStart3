d055b5e0e36e46bb1f2e7b105b443d735bc92d44
beverly-csu <80579574+beverly-csu@users.noreply.github.com>
2023-01-15 15:08:04 +0500

    Update catch_up.py

--- PyGame/Catch Up/catch_up.py
+++ PyGame/Catch Up/catch_up.py
@@ -12,8 +12,22 @@ x2, y2 = 500, 100
 sprite1 = transform.scale(image.load('PyGame/Catch Up/sprite1.png'), (100, 100))
 sprite2 = transform.scale(image.load('PyGame/Catch Up/sprite2.png'), (100, 100))
 
+clock = time.Clock()
+FPS = 60
+
 game = True
 while game:
+    keys = key.get_pressed()
+
+    if keys[K_RIGHT] and x2 < 595:
+        x2 += 10
+    if keys[K_LEFT] and x2 > 5:
+        x2 -= 10
+    if keys[K_DOWN] and y2 < 595:
+        y2 += 10
+    if keys[K_UP] and y2 > 5:
+        y2 -= 10
+
     window.blit(background, (0, 0))
     window.blit(sprite1, (x1, y1))
     window.blit(sprite2, (x2, y2))
@@ -22,4 +36,5 @@ while game:
         if e.type == QUIT:
             game = False
 
+    clock.tick(FPS)
     display.update()
