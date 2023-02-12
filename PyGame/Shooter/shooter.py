e07d77eba5c6ce3949828239619d23b4b8c65533
beverly-csu <80579574+beverly-csu@users.noreply.github.com>
2023-02-12 14:41:54 +0500

    Update shooter.py

--- PyGame/Shooter/shooter.py
+++ PyGame/Shooter/shooter.py
@@ -2,6 +2,7 @@ from pygame import *
 mixer.init()
 
 WIDTH, HEIGHT = 700, 500
+FPS = 60
 
 mw = display.set_mode((WIDTH, HEIGHT))
 display.set_caption('Shooter | шутер')
@@ -10,5 +11,17 @@ background = transform.scale(image.load('bg.jpg'), (WIDTH, HEIGHT))
 
 mixer.music.load('bg_music.ogg')
 mixer.music.play()
-
 fire = mixer.Sound('fire.ogg')
+
+clock = time.Clock()
+
+game = True
+while game:
+    mw.blit(background, (0, 0))
+
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    
+    display.update()
+    clock.tick(FPS)
