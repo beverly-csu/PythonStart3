305f2450c565fe40b6f9438256b403e3611f56c8
beverly-csu <80579574+beverly-csu@users.noreply.github.com>
2023-02-12 15:18:53 +0500

    update

--- PyGame/Shooter/shooter.py
+++ PyGame/Shooter/shooter.py
@@ -1,6 +1,30 @@
 from pygame import *
 mixer.init()
 
+class GameSprite(sprite.Sprite):
+    def __init__(self, img, x, y, width, height, speed):
+        self.speed = speed
+        self.image = transform.scale(image.load(img), (width, height))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def reset(self):
+        mw.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_a] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_d] and self.rect.x < (WIDTH - self.rect.width - 5):
+            self.rect.x += self.speed
+        if keys[K_SPACE]:
+            self.fire()
+
+    def fire(self):
+        fire_sound.play()
+
 WIDTH, HEIGHT = 700, 500
 FPS = 60
 
@@ -11,7 +35,7 @@ background = transform.scale(image.load('bg.jpg'), (WIDTH, HEIGHT))
 
 mixer.music.load('bg_music.ogg')
 mixer.music.play()
-fire = mixer.Sound('fire.ogg')
+fire_sound = mixer.Sound('fire.wav')
 
 clock = time.Clock()
 
